716c9e81f0a1bbf6689a8056eedb3d148a2c71a7
perjor <jordy.pereira@hotmail.com>
2018-12-07 23:50:54 +0100

    Group list

--- src/components/GroupInput.js
+++ src/components/GroupInput.js
@@ -20,24 +20,35 @@ class GroupInput extends Component {
 
   onSubmitHandler = (e) => {
     e.preventDefault();
-    this.setState({
-      groups: [
-        ...this.state.groups, 
-        this.state.groupName]
-    });
+    // if input is not empty then..
+      if(this.state.groupName !== ''){
+        this.setState({
+          groups: [
+            ...this.state.groups, 
+            this.state.groupName]
+        });
+    }
   }
 
   renderList() {
+
     console.log("rendering list");
-    return this.state.groups.map((group,id) => <li key={id}>{group}<button onClick={id => this.deleteGroupName(id)}>X</button> </li>);
+    
+    return this.state.groups.map((groupName,id) => 
+    <li key={id}>
+      {groupName}
+      <button class="btn btn-danger pl-3" onClick={(e) => this.deleteGroupName(e, id)}>X</button> 
+    </li>);
+    
+   
   }
-  deleteGroupName(id) {
-    console.log(' group name deleted');
-    const array = [...this.state.groups];
-    if (id > -1) {
-      array.splice(id, 1);
-      this.setState({groups: array});
-    }
+
+  deleteGroupName(e, id) {
+    this.setState({groups: this.state.groups.filter(
+      (groupName, i) => 
+        i !== id
+        )
+    });
   }
 
   render() {
@@ -48,7 +59,7 @@ class GroupInput extends Component {
           Groupname:
           <input type="text" name="name" onChange={e => this.onChangeHandler(e)}/>
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" class="btn btn-primary ml-1" />
       </form>
       {this.renderList()}
       </div>
