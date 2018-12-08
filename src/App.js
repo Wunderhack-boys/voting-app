c6d2b5a01bcc88bf86026db9879ab94f93be8667
LazloDB <lazlodebock@hotmail.com>
2018-12-08 01:26:04 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import Vote from './components/Vote.js';
+import Results from './components/Results.js';
 import GroupInput from './components/GroupInput.js';
 import { FirebaseContext } from './components/Firebase';
 import Login from './components/Login.js';
+import { Route } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Vote/>
         {/* <FirebaseContext.Consumer>
           {firebase => <GroupInput firebase={firebase}/>}
         </FirebaseContext.Consumer>
         <Login/> */}
+          <Route exact path='/' component={Vote}/>
+          <Route exact path='/da-resultz' component={Results}/>
+          {/* both /roster and /roster/:number begin with /roster */}
+          {/* <Route path='/roster' component={Roster}/> */}
       </div>
     );
   }
